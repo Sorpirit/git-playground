c5660a021df7beebf478eccd639571d831c5e92e
g.isachenko <george.isachenko@tatemgames.com>
2020-02-08 18:02:17 +0200

    Add text file reader instead of stub

--- nice-project/Program.cs
+++ nice-project/Program.cs
@@ -8,7 +8,7 @@ namespace NiceProject
         {
             Console.WriteLine("I'm doing some very important stuff!");
 
-            var fileReader = new EmptyFileReader();
+            var fileReader = new TextFileReader("../../../input.txt");
             
             var stuff = new ImportantStuff();
             var result = stuff.DoWhatNeedsToBeDone(fileReader);
